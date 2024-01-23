f0fd38d54740f8e2822ed22054093807c433ae76
Odoo Paas <paas-admin@odoo.com.test>
2024-01-23 15:16:18 +0000

    [CHANGE] Fix manifest file errors

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -1,10 +1,7 @@
 {
-    'name':  'Motorcycle Registry',
-    'summary': """Manage Registration of Motorcycles.""",
-    'description': """Motorcycle Regisrty
-    ================
-    This Modules is used to keep track of Motorcycle Registration and Ownership of each motorcycle of the brand.
-    """,
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles.',
+    'description': 'Motorcycle Regisrty\n================\nThis Modules is used to keep track of Motorcycle Registration and Ownership of each motorcycle of the brand.',
     'author': 'eiasaodoo',
     'version': '0.0.1',
     'licence': 'OPL-1',
@@ -13,5 +10,5 @@
     'depends': ['base'],
     'data': [],
     'demo': [],
-    'application': 'True',
+    'application': 'True'
 }
