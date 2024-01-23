d22fb5c1e8a27e8df8b2b30c64164a30084f1add
Odoo Paas <paas-admin@odoo.com.test>
2024-01-23 23:12:17 +0000

    [UPDATE]Fix motorcycleregistry.py sintax

--- motorcycle_registry/models/motorcycleregistry.py
+++ motorcycle_registry/models/motorcycleregistry.py
@@ -4,10 +4,10 @@ class MotorcycleRegistry(models.Model):
     _name = "motorcycle.registry"
     _description = "Motrcycle Registry"
 
-    certificate_title = fields.Binary(string="Titulo de Propiedad", required)
+    certificate_title = fields.Binary(string="Titulo de Propiedad", required=True)
     current_milleage = fields.Float(string="Millaje Actual")
-    name = fields.Char(string="Nombre")
-    last_name = fields.Char(string="Apellido")
+    name = fields.Char(string="Nombre", required=True)
+    last_name = fields.Char(string="Apellido", required=True)
     license_plate = fields.Char(string="Matricula")
     registry_date = fields.Date(string="Fecha de Registro")
-    vin = fields.Char(string="VIN")
+    vin = fields.Char(string="VIN", required=True)
