f4a2673e92568e70aec56bcd37d4c9cb8107c824
Odoo Paas <paas-admin@odoo.com.test>
2024-01-23 23:09:04 +0000

    [ADD] new fields into Motorcycle Registy module

---
+++ motorcycle_registry/models/motorcycleregistry.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+class MotorcycleRegistry(models.Model):
+    _name = "motorcycle.registry"
+    _description = "Motrcycle Registry"
+
+    certificate_title = fields.Binary(string="Titulo de Propiedad", required)
+    current_milleage = fields.Float(string="Millaje Actual")
+    name = fields.Char(string="Nombre")
+    last_name = fields.Char(string="Apellido")
+    license_plate = fields.Char(string="Matricula")
+    registry_date = fields.Date(string="Fecha de Registro")
+    vin = fields.Char(string="VIN")
