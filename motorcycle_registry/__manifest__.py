2236e05853536eef9e446e9260885c9a1747efca
eiasaodoo <andres.villegas@eiasa.com.co>
2024-01-23 14:24:37 +0000

    [ADD] Motorcycle Registry Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':  'Motorcycle Registry',
+    'summary': """Manage Registration of Motorcycles.""",
+    'description': """Motorcycle Regisrty
+    ================
+    This Modules is used to keep track of Motorcycle Registration and Ownership of each motorcycle of the brand.
+    """,
+    'author': 'eiasaodoo',
+    'version': '0.0.1',
+    'licence': 'OPL-1',
+    'website': 'https://github.com/eiasaodoo/kawillMotors.git',
+    'category': 'Kawill/Motorcycle Resgistry',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': 'True',  
+}
