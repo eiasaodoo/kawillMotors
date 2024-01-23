a7955aadfeeab075cb44ed01d15e88349fbb6985
Odoo Paas <paas-admin@odoo.com.test>
2024-01-23 17:05:40 +0000

    [UPDATE] Manifest file

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -4,7 +4,7 @@
     'description': 'Motorcycle Regisrty\n================\nThis Modules is used to keep track of Motorcycle Registration and Ownership of each motorcycle of the brand.',
     'author': 'eiasaodoo',
     'version': '0.0.1',
-    'licence': 'OPL-1',
+    'license': 'OPL-1',
     'website': 'https://github.com/eiasaodoo/kawillMotors.git',
     'category': 'Kawill/Motorcycle Resgistry',
     'depends': ['base'],
